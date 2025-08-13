f775750bacdc7dcbd7cc663fd01496c63f97b3df
RamonArents <ramonarents@hotmail.com>
2025-08-13 22:45:21 +0200

    init and dispose

---
+++ lib/views/pages/login_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/views/widgets/hero_widget.dart';
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    //Same as g_init. Runs on start
+    print('initState');
+    super.initState();
+  }
+  
+  @override
+  void dispose() {
+    //Here we can dispose of items that we don't need anymore
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            HeroWidget(title: 'Login'),
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
