b13c9ee78338808a5cfcc79f02f92de2ebc150ea
RamonArents <ramonarents@hotmail.com>
2025-08-15 18:17:21 +0200

    uit mods

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/views/widget_tree.dart';
 import 'package:flutter_tutorial/views/widgets/hero_widget.dart';
 
-
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({super.key, required this.title});
+
+  final String title;
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController controllerEm = TextEditingController(text: '123');
+  TextEditingController controllerPw = TextEditingController(text: '456');
 
-  TextEditingController controller = TextEditingController();
-  
+  String confirmedEmail = '123';
+  String confirmedPassword = '456';
+
   @override
   void dispose() {
     //Here we can dispose of items that we don't need anymore
-    controller.dispose();
+    controllerEm.dispose();
+    controllerPw.dispose();
     super.dispose();
   }
 
@@ -24,21 +30,70 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            HeroWidget(title: 'Login'),
-            TextField(
-              controller: controller,
-              decoration: InputDecoration(border: OutlineInputBorder()),
-              onEditingComplete: () {
-                setState(() {});
-              },
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                HeroWidget(title: widget.title),
+                SizedBox(height: 20.0),
+                TextField(
+                  controller: controllerEm,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(height: 10.0),
+                TextField(
+                  controller: controllerPw,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  onEditingComplete: () {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    onLoginPressed();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40),
+                  ),
+                  child: Text(widget.title),
+                ),
+                SizedBox(height: 50.0),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  void onLoginPressed() {
+    if (confirmedEmail == controllerEm.text &&
+        confirmedPassword == controllerPw.text) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return WidgetTree();
+          },
+        ),
+      );
+    }
   }
 }
