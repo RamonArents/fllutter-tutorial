67b318e6cf4d40bb00ce614a3609ea3ca31c8e50
RamonArents <ramonarents@hotmail.com>
2025-08-19 12:23:30 +0200

    expandend and flexible

--- lib/views/pages/expaned_flexible_page.dart
+++ lib/views/pages/expaned_flexible_page.dart
@@ -9,8 +9,44 @@ class ExpanedFlexiblePage extends StatelessWidget {
       appBar: AppBar(),
       body: Column(
         children: [
-          Expanded(child: Container(color: Colors.teal)),
-          Expanded(child: Container(color: Colors.orange)),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.teal,
+                  height: 20.0,
+                  child: Text('Hello'),
+                ),
+              ),
+              Flexible(
+                child: Container(
+                  color: Colors.orange,
+                  height: 20.0,
+                  child: Text('Hello'),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Flexible(
+                flex: 4,
+                child: Container(
+                  color: Colors.orange,
+                  height: 20.0,
+                  child: Text('Hello'),
+                ),
+              ),
+              Divider(),
+              Expanded(
+                child: Container(
+                  color: Colors.teal,
+                  height: 20.0,
+                  child: Text('Hello'),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
