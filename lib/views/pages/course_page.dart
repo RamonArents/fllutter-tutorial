3f7364af80d0c6bec95f79ec9372334b095e1ed8
RamonArents <ramonarents@hotmail.com>
2025-08-20 11:31:55 +0200

    working with api

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,8 +1,37 @@
+import 'dart:convert' as convert;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/views/widgets/hero_widget.dart';
+import 'package:http/http.dart' as http;
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData() async {
+    var url =
+      Uri.https('bored-api.appbrewery.com', '/random');
+
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var jsonResponse =
+        convert.jsonDecode(response.body) as Map<String, dynamic>;
+    var itemCount = jsonResponse['activity'];
+    print('Reponse API: $itemCount.');
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }  
+  }
 
   @override
   Widget build(BuildContext context) {
